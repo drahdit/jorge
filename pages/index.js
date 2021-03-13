fe6936de02d2080ed73eab53759a506cfb19edd7
drahdit <80552873+drahdit@users.noreply.github.com>
2021-03-13 11:10:02 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,8 @@
-import { useState } from 'react';
-
 function Home() {
-     return (
-        <div>
-            <h2>Home 2</h2>
-            <Contador />
-        </div>
-    )          
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-    
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onCLick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+     return ( <div>
+         <h1>Home</h1>
+         <a href="/sobre">Acessar página Sobre</a>
+     </div>)
 }
 
 export default Home
